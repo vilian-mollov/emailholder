a15324406ca3373ef487fe7006b77f7f1125801d
Avalon <vilian3737@gmail.com>
2023-05-24 14:12:48 +0300

    Updated SiteRepository to extend JpaRepository

--- src/main/java/com/emailspringproject/emailholder/repositories/SiteRepository.java
+++ src/main/java/com/emailspringproject/emailholder/repositories/SiteRepository.java
@@ -1,7 +1,7 @@
 package com.emailspringproject.emailholder.repositories;
 
 import com.emailspringproject.emailholder.domain.Site;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SiteRepository extends CrudRepository<Site, Long> {
+public interface SiteRepository extends JpaRepository<Site, Long> {
 }
