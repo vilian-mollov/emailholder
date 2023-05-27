8cd5d94c2898e5403f161ac3c4ae10d53182efe8
Avalon <vilian3737@gmail.com>
2023-05-27 18:12:00 +0300

    Updated EmailServiceImpl added logic for basic operations

--- src/main/java/com/emailspringproject/emailholder/services/impl/EmailServiceImpl.java
+++ src/main/java/com/emailspringproject/emailholder/services/impl/EmailServiceImpl.java
@@ -1,12 +1,15 @@
 package com.emailspringproject.emailholder.services.impl;
 
 import com.emailspringproject.emailholder.domain.Email;
+import com.emailspringproject.emailholder.domain.Site;
 import com.emailspringproject.emailholder.repositories.EmailRepository;
 import com.emailspringproject.emailholder.repositories.SiteRepository;
 import com.emailspringproject.emailholder.services.EmailService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -18,7 +21,7 @@ public class EmailServiceImpl implements EmailService {
         this.emailRepository = emailRepository;
         this.siteRepository = siteRepository;
     }
-
+    //TODO make validations
     @Override
     public List<Email> getAllEmails() {
         return emailRepository.findAll();
@@ -26,14 +29,25 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public Email getEmailById(Long id) {
-        return null;
+        return emailRepository.findById(id).orElse(null);
     }
 
     @Override
     public Email createEmail(Email email) {
-        return null;
+        return emailRepository.save(email);
     }
 
+    @Override
+    public Email createEmail(Long siteId, Email email) {
+        Site site = siteRepository.findById(siteId).orElse(null);
+        if (site != null) {
+            email.getSites().add(site); // Add the site to the email's sites set
+            site.getEmails().add(email); // Add the email to the site's emails set
+            return emailRepository.save(email);
+        }
+        return null;
+    }
+    //TODO implement the updateEmail method
     @Override
     public Email updateEmail(Long id, Email updatedEmail) {
         return null;
@@ -41,16 +55,44 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void deleteEmail(Long id) {
-
+        emailRepository.deleteById(id);
     }
 
     @Override
-    public void addSiteToEmail(Long emailId, Long siteId) {
+    public ResponseEntity<Email> addSiteToEmail(Long emailId, Long siteId) {
+        Optional<Email> optionalEmail = emailRepository.findById(emailId);
+        Optional<Site> optionalSite = siteRepository.findById(siteId);
 
+        if (optionalEmail.isPresent() && optionalSite.isPresent()) {
+            Email email = optionalEmail.get();
+            Site site = optionalSite.get();
+
+            email.addSite(site);
+            emailRepository.save(email);
+
+            return ResponseEntity.ok(email);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @Override
-    public void removeSiteFromEmail(Long emailId, Long siteId) {
+    public ResponseEntity<Email> removeSiteFromEmail(Long emailId, Long siteId) {
+
+        Optional<Email> optionalEmail = emailRepository.findById(emailId);
+        Optional<Site> optionalSite = siteRepository.findById(siteId);
+
+        if (optionalEmail.isPresent() && optionalSite.isPresent()) {
+            Email email = optionalEmail.get();
+            Site site = optionalSite.get();
+
+            email.removeSite(site);
+            emailRepository.save(email);
+
+            return ResponseEntity.ok(email);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
 
     }
 
