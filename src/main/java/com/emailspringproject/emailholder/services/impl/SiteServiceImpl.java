2c62baf0b47ded5debcd48ebbb223f27ac151a26
Avalon <vilian3737@gmail.com>
2023-05-27 16:32:43 +0300

    Initial SiteServiceImpl

--- src/main/java/com/emailspringproject/emailholder/services/impl/SiteServiceImpl.java
+++ src/main/java/com/emailspringproject/emailholder/services/impl/SiteServiceImpl.java
@@ -1,4 +1,53 @@
 package com.emailspringproject.emailholder.services.impl;
 
-public class SiteServiceImpl {
+import com.emailspringproject.emailholder.domain.Site;
+import com.emailspringproject.emailholder.repositories.EmailRepository;
+import com.emailspringproject.emailholder.repositories.SiteRepository;
+import com.emailspringproject.emailholder.services.SiteService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SiteServiceImpl implements SiteService {
+    private final SiteRepository siteRepository;
+    private final EmailRepository emailRepository;
+
+    // Constructor injection
+    public SiteServiceImpl(SiteRepository siteRepository, EmailRepository emailRepository) {
+        this.siteRepository = siteRepository;
+        this.emailRepository = emailRepository;
+    }
+
+    @Override
+    public List<Site> getAllSites() {
+        return siteRepository.findAll();
+    }
+
+    @Override
+    public Site getSiteById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Site createSite(Site site) {
+        return null;
+    }
+
+    @Override
+    public Site updateSite(Long id, Site updatedSite) {
+        return null;
+    }
+
+    @Override
+    public void deleteSite(Long id) {
+
+    }
+
+    @Override
+    public void addEmailToSite(Long siteId, Long emailId) {
+
+    }
+
+
 }
