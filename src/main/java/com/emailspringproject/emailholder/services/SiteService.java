27475d6c352bc3cffb12dcec8b8f0a5cca6fc675
Avalon <vilian3737@gmail.com>
2023-05-27 16:31:22 +0300

    Initial SiteService interface

--- src/main/java/com/emailspringproject/emailholder/services/SiteService.java
+++ src/main/java/com/emailspringproject/emailholder/services/SiteService.java
@@ -1,4 +1,14 @@
 package com.emailspringproject.emailholder.services;
 
+import com.emailspringproject.emailholder.domain.Site;
+
+import java.util.List;
+
 public interface SiteService {
+    List<Site> getAllSites();
+    Site getSiteById(Long id);
+    Site createSite(Site site);
+    Site updateSite(Long id, Site updatedSite);
+    void deleteSite(Long id);
+    void addEmailToSite(Long siteId, Long emailId);
 }
