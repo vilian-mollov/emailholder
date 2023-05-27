19be064d74b81e54bbc608e82561dc743bfce656
Avalon <vilian3737@gmail.com>
2023-05-27 13:13:05 +0300

    Updated HomeController

--- src/main/java/com/emailspringproject/emailholder/controllers/HomeController.java
+++ src/main/java/com/emailspringproject/emailholder/controllers/HomeController.java
@@ -1,23 +1,10 @@
 package com.emailspringproject.emailholder.controllers;
 
-
-import com.emailspringproject.emailholder.domain.Email;
-import com.emailspringproject.emailholder.repositories.EmailRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RestController
-@RequestMapping("/emails")
+@Controller
 public class HomeController {
-
-    private final EmailRepository emailRepository;
-
-    @Autowired
-    public HomeController(EmailRepository emailRepository){
-        this.emailRepository = emailRepository;
-    }
 
     @RequestMapping({"", "/", "/index.html"})
     public String getIndexPage(){
@@ -32,22 +19,6 @@ public class HomeController {
     @RequestMapping("/list.html")
     public String getListPage(){
         return "list";
-    }
-
-    @GetMapping
-    public List<Email> getEmails() {
-        return emailRepository.findAll();
-    }
-
-    @PostMapping
-    public Email createUser(@RequestBody Email email) {
-        return emailRepository.save(email);
-    }
-
-    @PutMapping("/{id}")
-    public Email updateEmail(@PathVariable Long id, @RequestBody Email email) {
-        email.setId(id);
-        return emailRepository.save(email);
     }
 
 }
