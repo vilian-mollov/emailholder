2fedca3b31ce32fe90432561e465dcfc6d61759a
Avalon <vilian3737@gmail.com>
2023-05-27 16:31:47 +0300

    Initial EmailService interface

--- src/main/java/com/emailspringproject/emailholder/services/EmailService.java
+++ src/main/java/com/emailspringproject/emailholder/services/EmailService.java
@@ -1,4 +1,15 @@
 package com.emailspringproject.emailholder.services;
 
+import com.emailspringproject.emailholder.domain.Email;
+
+import java.util.List;
+
 public interface EmailService {
+    List<Email> getAllEmails();
+    Email getEmailById(Long id);
+    Email createEmail(Email email);
+    Email updateEmail(Long id, Email updatedEmail);
+    void deleteEmail(Long id);
+    void addSiteToEmail(Long emailId, Long siteId);
+    void removeSiteFromEmail(Long emailId, Long siteId);
 }
