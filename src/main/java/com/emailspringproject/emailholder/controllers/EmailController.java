ce25d678538a0bd31701aedf9ce8e66eae53d0ee
Avalon <vilian3737@gmail.com>
2023-05-27 18:14:31 +0300

    Updated EmailController delegated the implementation of the business operations to the service layer

--- src/main/java/com/emailspringproject/emailholder/controllers/EmailController.java
+++ src/main/java/com/emailspringproject/emailholder/controllers/EmailController.java
@@ -2,15 +2,14 @@ package com.emailspringproject.emailholder.controllers;
 
 
 import com.emailspringproject.emailholder.domain.Email;
-import com.emailspringproject.emailholder.domain.Site;
 import com.emailspringproject.emailholder.repositories.EmailRepository;
 import com.emailspringproject.emailholder.repositories.SiteRepository;
+import com.emailspringproject.emailholder.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/emails")
@@ -19,72 +18,47 @@ public class EmailController {
 
     private final EmailRepository emailRepository;
     private final SiteRepository siteRepository;
+    private final EmailService emailService;
 
     @Autowired
-    public EmailController(EmailRepository emailRepository,SiteRepository siteRepository){
+    public EmailController(EmailRepository emailRepository,SiteRepository siteRepository, EmailService emailService){
         this.emailRepository = emailRepository;
         this.siteRepository = siteRepository;
+        this.emailService = emailService;
     }
 
     @GetMapping
     public List<Email> getAllEmails() {
-        return emailRepository.findAll();
+        return emailService.getAllEmails();
     }
 
     @GetMapping("/{id}")
     public Email getEmailById(@PathVariable Long id) {
-        return emailRepository.findById(id).orElse(null);
+        return emailService.getEmailById(id);
     }
 
     @PostMapping("/{siteId}")
     public Email createEmail(@PathVariable Long siteId, @RequestBody Email email) {
-        Site site = siteRepository.findById(siteId).orElse(null);
-        if (site != null) {
-            email.getSites().add(site); // Add the site to the email's sites set
-            site.getEmails().add(email); // Add the email to the site's emails set
-            return emailRepository.save(email);
-        }
-        return null;
+        return emailService.createEmail(siteId,email);
+    }
+
+    @PostMapping("/create")
+    public Email createEmail(@RequestBody Email email) {
+            return emailService.createEmail(email);
     }
 
     @PutMapping("/{emailId}/sites/{siteId}")
     public ResponseEntity<Email> addSiteToEmail(@PathVariable Long emailId, @PathVariable Long siteId) {
-        Optional<Email> optionalEmail = emailRepository.findById(emailId);
-        Optional<Site> optionalSite = siteRepository.findById(siteId);
-
-        if (optionalEmail.isPresent() && optionalSite.isPresent()) {
-            Email email = optionalEmail.get();
-            Site site = optionalSite.get();
-
-            email.addSite(site);
-            emailRepository.save(email);
-
-            return ResponseEntity.ok(email);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return emailService.addSiteToEmail(emailId,siteId);
     }
 
     @DeleteMapping("/{emailId}/sites/{siteId}")
     public ResponseEntity<Email> removeSiteFromEmail(@PathVariable Long emailId, @PathVariable Long siteId) {
-        Optional<Email> optionalEmail = emailRepository.findById(emailId);
-        Optional<Site> optionalSite = siteRepository.findById(siteId);
-
-        if (optionalEmail.isPresent() && optionalSite.isPresent()) {
-            Email email = optionalEmail.get();
-            Site site = optionalSite.get();
-
-            email.removeSite(site);
-            emailRepository.save(email);
-
-            return ResponseEntity.ok(email);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return emailService.removeSiteFromEmail(emailId,siteId);
     }
 
     @DeleteMapping("/{id}")
     public void deleteEmail(@PathVariable Long id) {
-        emailRepository.deleteById(id);
+        emailService.deleteEmail(id);
     }
 }
