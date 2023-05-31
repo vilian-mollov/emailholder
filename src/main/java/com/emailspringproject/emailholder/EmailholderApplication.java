f14641d2f2117c2a1dc7e8fef25a0e48de97e2a0
Avalon <vilian3737@gmail.com>
2023-05-31 22:21:51 +0300

    Updated EmailholderApplication added new annotations

--- src/main/java/com/emailspringproject/emailholder/EmailholderApplication.java
+++ src/main/java/com/emailspringproject/emailholder/EmailholderApplication.java
@@ -2,8 +2,12 @@ package com.emailspringproject.emailholder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableSpringDataWebSupport
 public class EmailholderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmailholderApplication.class, args);
