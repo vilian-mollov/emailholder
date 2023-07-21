6c2827d49329ebaad7a0c743d5137708a8cc6eac
Avalon <vilian3737@gmail.com>
2023-07-21 16:01:54 +0300

    Fixed Thymeleaf not fetching css, js and images from static directory by deleting annotations in EmailholderApplication class

--- src/main/java/com/emailspringproject/emailholder/EmailholderApplication.java
+++ src/main/java/com/emailspringproject/emailholder/EmailholderApplication.java
@@ -2,12 +2,8 @@ package com.emailspringproject.emailholder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
-@EnableSpringDataWebSupport
 public class EmailholderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmailholderApplication.class, args);
