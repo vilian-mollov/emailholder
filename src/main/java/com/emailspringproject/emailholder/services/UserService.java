85f753a00837002d651a212592a2f0b61db30df2
Avalon <vilian3737@gmail.com>
2023-08-27 21:30:55 +0300

    Added UserService interface and implementation class

---
+++ src/main/java/com/emailspringproject/emailholder/services/UserService.java
@@ -0,0 +1,12 @@
+package com.emailspringproject.emailholder.services;
+
+public interface UserService {
+
+    void loginUser();
+
+    void registerUser();
+
+    void updateUser();
+
+    void deleteUser();
+}
