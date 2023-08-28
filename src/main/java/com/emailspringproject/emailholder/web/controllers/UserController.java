42829a9bc5013ddf3e2e1cd452ecf6f0209cdf76
Avalon <vilian3737@gmail.com>
2023-08-28 20:40:05 +0300

    Implemented methods structure for basic CRUD operations

--- src/main/java/com/emailspringproject/emailholder/web/controllers/UserController.java
+++ src/main/java/com/emailspringproject/emailholder/web/controllers/UserController.java
@@ -3,8 +3,7 @@ package com.emailspringproject.emailholder.web.controllers;
 import com.emailspringproject.emailholder.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -25,6 +24,41 @@ public class UserController {
         modelAndView.setViewName("redirect:/index");
 
         // TODO add userService.loginUser()
+
+        return modelAndView;
+    }
+
+
+    @PostMapping("/register")
+    public ModelAndView registerUser() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/index");
+
+        // TODO add userService.registerUser()
+
+        return modelAndView;
+    }
+
+
+    @PostMapping("/update")
+    public ModelAndView updateUser() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/index");
+
+        // TODO add userService.updateUser()
+
+        return modelAndView;
+    }
+
+    @DeleteMapping("/delete")
+    public ModelAndView deleteUser() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/index");
+
+        // TODO add userService.deleteUser()
 
         return modelAndView;
     }
