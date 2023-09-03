1d1100af7298b220e975139a83156e1ebb4cb7af
Avalon <vilian3737@gmail.com>
2023-09-03 16:41:29 +0300

    Created three blocks of content in the content section and implemented the show on scroll functionality Implemented get started button functionality to scroll down

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,11 +1,14 @@
 let themeBtn = document.getElementById("theme_btn");
 let menu = document.getElementById("menu");
 let menu_list = document.getElementById("menu_list");
+let mainGetStartBtn = document.getElementById("start_btn");
 let buttonIsOn = false;
 
-function addHandlers(){
+function addHandlers() {
     themeBtn.addEventListener("click", changeTheme);
     menu.addEventListener("click", toggleMenu);
+    window.addEventListener("scroll", showHiddenContext)
+    mainGetStartBtn.addEventListener("click",scrollDownMainBtn);
 }
 
 
@@ -13,13 +16,13 @@ function changeTheme() {
 
     let icon = document.createElement('img');
 
-    if(buttonIsOn) {
+    if (buttonIsOn) {
         icon.src = "images/toggle-off-solid.svg";
         icon.alt = "toggle off";
 
         buttonIsOn = false;
 
-    }else {
+    } else {
         icon.src = "images/toggle-on-solid.svg";
         icon.alt = "toggle on";
 
@@ -29,20 +32,19 @@ function changeTheme() {
     themeBtn.removeChild(themeBtn.firstChild);
 
     themeBtn.appendChild(icon);
-  }
+}
 
- 
 
 function toggleMenu() {
 
     let state;
 
-    if(menu_list.classList.contains('show')){
+    if (menu_list.classList.contains('show')) {
 
         state = 'disappear';
         console.log(state);
 
-    }else {
+    } else {
 
         state = 'show';
         console.log(state);
@@ -51,8 +53,41 @@ function toggleMenu() {
 
     menu_list.className = '';
     menu_list.classList.add(state);
-   
-  }
+
+}
+
+function showHiddenContext() {
+
+    const scrollPosition = window.scrollY;
+    const revealFirst = 530;
+    const revealSecond = 700;
+    const revealThird = 750;
+
+    let leftContent1 = document.getElementById("left-side-con");
+    let rightContent = document.getElementById("right-side-con");
+    let leftContent2 = document.getElementById("left-side-con2");
+
+    if (scrollPosition >= revealFirst) {
+        leftContent1.className = "";
+        leftContent1.classList.add("show");
+    }
+
+    if (scrollPosition >= revealSecond) {
+        rightContent.className = "";
+        rightContent.classList.add("show");
+    }
+
+    if (scrollPosition >= revealThird) {
+        leftContent2.className = "";
+        leftContent2.classList.add("show");
+    }
+
+}
+
+
+function scrollDownMainBtn() {
+    window.scrollTo(0,760);
+}
 
 
 addHandlers();
