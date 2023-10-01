c8d73a838aa4b173820b633a67956cc5ac43d28e
Avalon <vilian3737@gmail.com>
2023-10-01 17:37:41 +0300

    Imported spring security dependency Created bean

--- src/main/java/com/emailspringproject/emailholder/config/ApplicationBeanConfiguration.java
+++ src/main/java/com/emailspringproject/emailholder/config/ApplicationBeanConfiguration.java
@@ -5,6 +5,8 @@ import com.google.gson.GsonBuilder;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @Configuration
 public class ApplicationBeanConfiguration {
@@ -17,6 +19,11 @@ public class ApplicationBeanConfiguration {
     @Bean
     public Gson gson(){
         return new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 
 
